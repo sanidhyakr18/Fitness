beae829a8d2e644fa1a96376aab14d03018ee5e8
sanidhyakr18 <sanidhyakr18@gmail.com>
2021-11-28 06:00:04 +0530

    implement filter action

--- app/src/main/java/com/sandystudios/fitness/ApiInterface.kt
+++ app/src/main/java/com/sandystudios/fitness/ApiInterface.kt
@@ -4,6 +4,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiInterface {
-    @GET("all?category_id=14")
+    //    @GET("all?category_id=14")
+    @GET("all")
     suspend fun getCardio(): Response<APIResponse>
 }
--- app/src/main/java/com/sandystudios/fitness/MainActivity.kt
+++ app/src/main/java/com/sandystudios/fitness/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.sandystudios.fitness
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.android.material.navigation.NavigationView
@@ -13,7 +16,10 @@ import kotlinx.coroutines.*
 @DelicateCoroutinesApi
 class MainActivity : AppCompatActivity() {
 
+    private var originalList = arrayListOf<DataItem>()
     private val adapter = CardioAdapter()
+
+    lateinit var difficulty: String
 
     private val filterDrawerLayout: DrawerLayout by lazy {
         findViewById(R.id.filter_drawer_layout)
@@ -31,6 +37,12 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.img_apply)
     }
 
+    private val difficultySpinner: Spinner by lazy {
+        findViewById(R.id.difficulty_spinner)
+    }
+
+    private val difficulty_labels = arrayListOf("Any", "Beginner", "Intermediate", "Advanced")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -47,25 +59,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         imgApply.setOnClickListener {
+            difficulty = difficultySpinner.selectedItem.toString()
+            Log.d(TAG, difficulty)
             filterDrawerLayout.closeDrawer(filterView)
+            loadItems()
         }
 
-        GlobalScope.launch(Dispatchers.Main) {
-            try {
-                val response = withContext(Dispatchers.IO) { ApiClient.api.getCardio() }
-                if (response.isSuccessful) {
-                    response.body()?.let { response1 ->
-                        response1.data?.let {
-                            adapter.data = it as List<DataItem>
-                            adapter.notifyDataSetChanged()
-                        }
-                    }
-                }
-            } catch (e: Exception) {
-                adapter.data = emptyList()
-                adapter.notifyDataSetChanged()
-            }
-        }
+        setUpSpinner()
+
+        loadItems()
+    }
+
+    private fun setUpSpinner() {
+        val adapter = ArrayAdapter(this, R.layout.item_spinner, R.id.text, difficulty_labels)
+        difficultySpinner.adapter = adapter
+        difficulty = difficultySpinner.selectedItem.toString()
     }
 
     override fun onBackPressed() {
@@ -73,6 +81,36 @@ class MainActivity : AppCompatActivity() {
             filterDrawerLayout.closeDrawer(filterView)
         } else {
             super.onBackPressed()
+        }
+    }
+
+    fun loadItems() {
+        GlobalScope.launch(Dispatchers.Main) {
+            try {
+                val response = withContext(Dispatchers.IO) { ApiClient.api.getCardio() }
+                if (response.isSuccessful) {
+                    response.body()?.let { response1 ->
+                        response1.data?.let { it ->
+                            originalList.clear()
+                            originalList = it as ArrayList<DataItem>
+                            if(difficulty != "Any") {
+                                val getList = originalList.filter {
+                                    it.difficultyLevelName!!.contains(difficulty, true)
+                                }
+                                adapter.data = getList
+                                adapter.notifyDataSetChanged()
+                            } else {
+                                adapter.data = originalList
+                                adapter.notifyDataSetChanged()
+                            }
+
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                adapter.data = emptyList()
+                adapter.notifyDataSetChanged()
+            }
         }
     }
 }
